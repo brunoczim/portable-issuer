06b93f823b6dc5f14015bd7403d072e3fb1f7408
brunoczim <brunoczim@gmail.com>
2024-07-27 23:13:14 -0300

    redirect root to static index

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,11 @@
 use std::path::PathBuf;
 
-use axum::Router;
+use axum::{
+    http::{HeaderMap, HeaderName, HeaderValue, StatusCode},
+    response::{IntoResponse, Response},
+    routing::get,
+    Router,
+};
 
 mod status;
 mod api;
@@ -10,4 +15,13 @@ pub fn router(static_path: impl Into<PathBuf>) -> Router {
     Router::new()
         .nest("/api/v1/", api::router())
         .nest("/static/", static_files::router(static_path))
+        .route("/", get(get_root))
+}
+
+async fn get_root() -> impl IntoResponse {
+    (
+        StatusCode::PERMANENT_REDIRECT,
+        [("Location", "/static/index.html")],
+        "Permanent redirect",
+    )
 }
